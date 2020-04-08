547cb40586b889496133ab09c45b6e86c37a45c3
Julio César Martínez <juliocesarmartnoza2111@gmail.com>
2020-04-07 21:49:07 -0600

    Commit 1.0 Nombre 7/4/2020 21:48
    Agregé un archivo .java en el package com.AvalesWebAppJavaClass

---
+++ WebApplication2/src/java/com/AvalesWebAppJavaClass/myName.java
@@ -0,0 +1,17 @@
+
+package com.AvalesWebAppJavaClass;
+
+
+public class myName 
+{
+    
+    private String nombre;
+    
+    public String nombre(){
+        this.nombre="papucho";
+        return nombre;
+    }
+    
+  
+    
+}

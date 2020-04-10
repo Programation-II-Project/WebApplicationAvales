79883dec2bd1803bf63a0c23dc9c10cdf6b73337
Kabel <abel_acc@outlook.com>
2020-04-09 18:30:31 -0600

    Bien
    .

--- WebApplication2/src/java/com/AvalesWebAppServlet/ServletUsuario.java
+++ WebApplication2/src/java/com/AvalesWebAppServlet/ServletUsuario.java
@@ -71,7 +71,7 @@ public class ServletUsuario extends HttpServlet
                     }
                         else
                     {
-                        request.getRequestDispatcher("errorBorrarSolicitud")
+                        request.getRequestDispatcher("errorBorrarSolicitud.jsp")
                         .forward(request, response);
                     }
             }
